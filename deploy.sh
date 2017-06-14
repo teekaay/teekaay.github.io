31487249187cdc70a4804b612b45407560faaabc
Thomas Klinger <Thomas.Klinger@otto.de>
2017-06-15 00:15:11 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ echo "Deploying"
 
 hugo
 git checkout master
+rm -rf about blog categories code colophon content css fixed img tags
+rm 404.html index.html sitemap.xml index.xml
 mv public/* ./
 rm -rf public
 git add .
 git commit -m 'Update website'
 git push origin master
 git checkout content
+
